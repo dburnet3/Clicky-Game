57bab9dffa030132e68cf17d2e5ff7629e25477f
dburnet3 <deniseburnett26@gmail.com>
2019-08-25 16:13:43 -0400

    updated header component

---
+++ my-app/src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+    render() {
+        return (
+
+            <div>
+                <img src='/assets/daria_banner.png' className="daria" alt='Daria Logo' />
+
+            </div>
+
+        );
+    }
+
+}
+
+export default Header;
