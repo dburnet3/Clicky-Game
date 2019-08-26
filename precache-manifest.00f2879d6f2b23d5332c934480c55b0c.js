bef7638c34cddbaceb4715b0dd7804e1130884a4
dburnet3 <deniseburnett26@gmail.com>
2019-08-26 19:46:03 -0400

    Updates

---
+++ precache-manifest.00f2879d6f2b23d5332c934480c55b0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee02297167575fd4b445bcf0fc572ecc",
+    "url": "/dburnet3/Clicky-Game/index.html"
+  },
+  {
+    "revision": "c91120e28ea8a771d25e",
+    "url": "/dburnet3/Clicky-Game/static/css/main.37fee97d.chunk.css"
+  },
+  {
+    "revision": "7460f6bbc54b4bf9b147",
+    "url": "/dburnet3/Clicky-Game/static/js/2.03a9568a.chunk.js"
+  },
+  {
+    "revision": "c91120e28ea8a771d25e",
+    "url": "/dburnet3/Clicky-Game/static/js/main.42eca761.chunk.js"
+  },
+  {
+    "revision": "90fa3a078f447bd79d47",
+    "url": "/dburnet3/Clicky-Game/static/js/runtime~main.4bac158d.js"
+  }
+]);
